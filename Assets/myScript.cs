a5846d120e0e189f2efeef8e411c90f078189184
Gavin Clarkson <user1@users-MacBook-Air.local>
2017-12-30 23:06:28 -0600

    saved project

---
+++ Assets/myScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class myScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print ("Gavin is cool");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
